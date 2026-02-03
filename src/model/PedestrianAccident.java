5342b1f260bb607e7d3c6955e8dfe61f9b5c5fc1
denjieatsmakima <aminatazibaeva71@gmail.com>
2026-02-03 20:27:50 +0500

    Update PedestrianAccident.java

--- src/model/PedestrianAccident.java
+++ src/model/PedestrianAccident.java
@@ -1,32 +1,28 @@
 package model;
 
-public class PedestrianAccident extends AccidentEvent{
+public class PedestrianAccident extends AccidentEvent {
     private boolean pedestrianInjured;
 
-    public PedestrianAccident(int id,String location, double latitude, double longitude, int severity, SensorData sensorData, boolean pedestrianInjured){
+    public PedestrianAccident(int id, String location, double latitude, double longitude, int severity,
+                              SensorData sensorData, boolean pedestrianInjured) {
         super(id, location, latitude, longitude, severity, sensorData);
-        this.pedestrianInjured=pedestrianInjured;
+        this.pedestrianInjured = pedestrianInjured;
     }
 
-    public boolean isPedestrianInjured() {
-        return pedestrianInjured;
-    }
+    public boolean isPedestrianInjured() { return pedestrianInjured; }
+    public void setPedestrianInjured(boolean pedestrianInjured) { this.pedestrianInjured = pedestrianInjured; }
 
-    public void setPedestrianInjured(boolean pedestrianInjured){
-        this.pedestrianInjured=pedestrianInjured;
+    @Override
+    public String getAccidentType() { return "PEDESTRIAN_ACCIDENT"; }
+
+    @Override
+    public void processAccident() {
+        System.out.println("Processing pedestrian accident. Injured: " + (pedestrianInjured ? "YES" : "NO"));
     }
 
     @Override
-    public String getAccidentType(){
-        return "PEDSTRIAN_ACCIDENT";
-    }
-
-    @Override
-    public void processAccident(){
-        if(pedestrianInjured){
-            System.out.println("Processing pedestrian accident: pedesttrian injured");
-        } else {
-            System.out.println("Processing pedestrian accident: no injures");
-        }
+    public String getAccidentSummary() {
+        return "PedestrianAccident{id=" + getId() + ", location='" + getLocation() + "', severity=" + getSeverity() +
+                ", injured=" + pedestrianInjured + "}";
     }
 }
