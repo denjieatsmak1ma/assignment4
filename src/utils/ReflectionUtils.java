3cef14825e95c2f4c4ea667e08f050c989c5ca03
denjieatsmakima <aminatazibaeva71@gmail.com>
2026-02-03 20:31:06 +0500

    Update ReflectionUtils.java

--- src/utils/ReflectionUtils.java
+++ src/utils/ReflectionUtils.java
@@ -1,4 +1,22 @@
 package utils;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class ReflectionUtils {
+
+    public static void inspectObject(Object obj) {
+        Class<?> c = obj.getClass();
+        System.out.println("Class: " + c.getName());
+
+        System.out.println("Fields:");
+        for (Field f : c.getDeclaredFields()) {
+            System.out.println("- " + f.getName() + " : " + f.getType().getSimpleName());
+        }
+
+        System.out.println("Methods:");
+        for (Method m : c.getDeclaredMethods()) {
+            System.out.println("- " + m.getName());
+        }
+    }
 }
