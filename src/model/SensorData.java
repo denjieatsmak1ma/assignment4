72c88d89e2705cd9d3fe6c9b08c1943474e99926
denjieatsmakima <aminatazibaeva71@gmail.com>
2026-02-03 20:28:48 +0500

    Update SensorData.java

--- src/model/SensorData.java
+++ src/model/SensorData.java
@@ -3,41 +3,36 @@ package model;
 import java.time.LocalDateTime;
 
 public class SensorData {
+    private int id; // будет из БД
     private double speed;
     private double acceleration;
     private LocalDateTime timestamp;
 
-    public SensorData(double speed, double acceleration, LocalDateTime timestamp){
-        this.speed=speed;
-        this.acceleration=acceleration;
-        this.timestamp=timestamp;
-    }
-
-    public double getAcceleration() {
-        return acceleration;
-    }
-    public void setAcceleration(double acceleration) {
-        this.acceleration = acceleration;
-    }
-
-    public double getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(double speed) {
+    public SensorData(int id, double speed, double acceleration, LocalDateTime timestamp) {
+        this.id = id;
         this.speed = speed;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
+        this.acceleration = acceleration;
         this.timestamp = timestamp;
     }
 
+    public SensorData(double speed, double acceleration, LocalDateTime timestamp) {
+        this(0, speed, acceleration, timestamp);
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public double getSpeed() { return speed; }
+    public void setSpeed(double speed) { this.speed = speed; }
+
+    public double getAcceleration() { return acceleration; }
+    public void setAcceleration(double acceleration) { this.acceleration = acceleration; }
+
+    public LocalDateTime getTimestamp() { return timestamp; }
+    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
+
     @Override
-    public String toString(){
-        return "SensorData{" + "speed="+ speed + ", acceleration=" + acceleration+ ", timestamp=" + timestamp+ "}";
+    public String toString() {
+        return "SensorData{speed=" + speed + ", acceleration=" + acceleration + ", timestamp=" + timestamp + "}";
     }
 }
