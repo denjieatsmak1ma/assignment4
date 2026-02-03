9c6c2c9bdea2489100c2f4e9220b7f45aa3d5b95
denjieatsmakima <aminatazibaeva71@gmail.com>
2026-02-03 20:40:32 +0500

    Update AccidentService.java

--- src/service/interfaces/AccidentService.java
+++ src/service/interfaces/AccidentService.java
@@ -1,4 +1,15 @@
 package service.interfaces;
 
-public class AccidentService {
+import exception.DuplicateResourceException;
+import model.AccidentEvent;
+
+import java.util.List;
+
+public interface AccidentService {
+    void addAccident(AccidentEvent accident) throws DuplicateResourceException;
+    AccidentEvent getAccidentById(int id);
+    void updateAccident(AccidentEvent accident);
+    void deleteAccident(int id);
+    List<AccidentEvent> getAllAccidentsSortedBySeverity();
 }
+
