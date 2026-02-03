045620621623d205dd972d25aac83adc4b12f4ed
denjieatsmakima <aminatazibaeva71@gmail.com>
2026-02-03 20:42:31 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import controller.AccidentController;
+import exception.DuplicateResourceException;
+import exception.InvalidInputException;
+import model.*;
+import repository.AccidentRepositoryImpl;
+import service.AccidentServiceImpl;
+import service.interfaces.AccidentService;
+import utils.ReflectionUtils;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+
+        AccidentRepositoryImpl repo = new AccidentRepositoryImpl();
+        AccidentService service = new AccidentServiceImpl(repo);
+        AccidentController controller = new AccidentController(service);
+
+        SensorData s1 = new SensorData(60.5, 2.3, LocalDateTime.now());
+        SensorData s2 = new SensorData(30.0, 0.7, LocalDateTime.now());
+
+        AccidentEvent a1 = new VehicleCollision(0, "Main street", 51.10, 71.40, 5, s1, 2);
+        AccidentEvent a2 = new PedestrianAccident(0, "Central park", 51.20, 71.30, 7, s2, true);
+
+        try {
+            controller.addAccident(a1);
+            controller.addAccident(a2);
+        } catch (DuplicateResourceException e) {
+            System.out.println("Caught DuplicateResourceException: " + e.getMessage());
+        } catch (InvalidInputException e) {
+            System.out.println("Caught InvalidInputException: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Caught other error: " + e.getMessage());
+        }
+
+        System.out.println("\nReflection");
+        ReflectionUtils.inspectObject(a1);
     }
 }
