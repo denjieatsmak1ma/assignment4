28f83c412167c56b832e094903241813c5cba6ca
denjieatsmakima <aminatazibaeva71@gmail.com>
2026-02-03 20:30:30 +0500

    Update VehicleCollision.java

--- src/model/VehicleCollision.java
+++ src/model/VehicleCollision.java
@@ -1,31 +1,31 @@
 package model;
 
-public class VehicleCollision extends AccidentEvent{
+public class VehicleCollision extends AccidentEvent {
     private int vehiclesInvolved;
 
-    public VehicleCollision(int id,String location,double latitude, double longitude, int severity, SensorData sensorData, int vehiclesInvolved){
-        super(id,location,latitude,longitude,severity,sensorData);
-        this.vehiclesInvolved=vehiclesInvolved;
+    public VehicleCollision(int id, String location, double latitude, double longitude, int severity,
+                            SensorData sensorData, int vehiclesInvolved) {
+        super(id, location, latitude, longitude, severity, sensorData);
+        setVehiclesInvolved(vehiclesInvolved);
     }
 
-    public int getVehiclesInvolved(){
-        return vehiclesInvolved;
-    }
-
-    public void setVehiclesInvolved(int vehiclesInvolved){
-        if(vehiclesInvolved<1){
-            throw new IllegalArgumentException("Vehicless involved must be at least 1");
-        }
-        this.vehiclesInvolved=vehiclesInvolved;
+    public int getVehiclesInvolved() { return vehiclesInvolved; }
+    public void setVehiclesInvolved(int vehiclesInvolved) {
+        if (vehiclesInvolved < 1) throw new IllegalArgumentException("Vehicles involved must be at least 1");
+        this.vehiclesInvolved = vehiclesInvolved;
     }
 
     @Override
-    public String getAccidentType(){
-        return "VEHICLE_COLLISION";
+    public String getAccidentType() { return "VEHICLE_COLLISION"; }
+
+    @Override
+    public void processAccident() {
+        System.out.println("Processing vehicle collision. Vehicles involved: " + vehiclesInvolved);
     }
 
     @Override
-    public void processAccident(){
-        System.out.println("Processing vehicle collision with " + vehiclesInvolved + " vehicles involved");
+    public String getAccidentSummary() {
+        return "VehicleCollision{id=" + getId() + ", location='" + getLocation() + "', severity=" + getSeverity() +
+                ", vehicles=" + vehiclesInvolved + "}";
     }
 }
