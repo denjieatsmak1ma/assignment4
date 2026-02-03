1ae9a6f2d28649d32bc33eaf6e69944357c3179a
denjieatsmakima <aminatazibaeva71@gmail.com>
2026-02-03 20:31:33 +0500

    Update SortingUtils.java

--- src/utils/SortingUtils.java
+++ src/utils/SortingUtils.java
@@ -1,4 +1,12 @@
 package utils;
 
+import model.AccidentEvent;
+
+import java.util.Comparator;
+import java.util.List;
+
 public class SortingUtils {
+    public static void sortBySeverity(List<AccidentEvent> list) {
+        list.sort(Comparator.comparingInt(AccidentEvent::getSeverity));
+    }
 }
