817d725a24bd155a81fe96b253804af2fdb26179
denjieatsmakima <aminatazibaeva71@gmail.com>
2026-02-03 20:36:13 +0500

    Update CrudRepository.java

--- src/repository/interfaces/CrudRepository.java
+++ src/repository/interfaces/CrudRepository.java
@@ -1,4 +1,11 @@
 package repository.interfaces;
 
-public class CrudRepository {
+import java.util.List;
+
+public interface CrudRepository<T> {
+    void create(T entity);
+    T read(int id);
+    void update(T entity);
+    void delete(int id);
+    List<T> findAll();
 }
